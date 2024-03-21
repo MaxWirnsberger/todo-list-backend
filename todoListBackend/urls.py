04b5a981b4f0eeab28b50b2b01302893aceeefe5
Max Wirnsberger <maximilian.wirnsberger@gmail.com>
2024-03-21 17:55:05 +0100

    final push

--- todoListBackend/urls.py
+++ todoListBackend/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for todoListBackend project.
+"""todolistbackend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todolist.views import LoginView, TodoItemsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view()),
+    path('todos/', TodoItemsView.as_view()),
 ]
